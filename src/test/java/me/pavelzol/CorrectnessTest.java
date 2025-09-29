16e67e12066827a100c8ee0e690c35318a1d4db8
PavelZol <zolpav@yandex.ru>
2025-09-29 16:12:14 +0200

    Fix correctness Test

--- src/test/java/me/pavelzol/CorrectnessTest.java
+++ src/test/java/me/pavelzol/CorrectnessTest.java
@@ -13,27 +13,26 @@ public class CorrectnessTest {
         UnSynchronizedAccount account = new UnSynchronizedAccount(0);
         try (var executor = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors() * CPU_MULTIPLIER)) {
-            executor.submit(() -> {
-                try {
-                    countDownLatch.await();
-                    for (int i = 0; i < 10; i++) {
-                        for (int j = 0; j < 2; j++) {
-                            int finalI = i;
-                            int finalJ = j;
+            for (int i = 0; i < 10_000; i++) {
+                for (int j = 0; j < 2; j++) {
+                    int finalI = i;
+                    int finalJ = j;
+                    executor.submit(() -> {
+                        try {
+                            countDownLatch.await();
                             if (finalJ % 2 == 0) {
                                 account.credit(finalI);
                             } else {
                                 account.debit(finalI);
                             }
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
                         }
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    });
                 }
-            });
+            }
+            countDownLatch.countDown();
         }
-        countDownLatch.countDown();
         Assertions.assertEquals(0D, account.getBalance());
     }
 }
-
